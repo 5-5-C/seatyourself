c4175ff17a5e121262fe5aaa0a1b7ef8e109ae61
Graham Moss <graham.moss@hotmail.com>
2017-02-07 14:22:22 -0500

    removed user_id from users table, configured app controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations
-  has_many :restaurants though: :reservations
-  has_many :owned_restaurants, class_name "restaurants"
+  has_many :restaurants, through: :reservations
   has_secure_password
   validates :name, :email, :password_digest, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207184747) do
+ActiveRecord::Schema.define(version: 20170207191852) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20170207184747) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
