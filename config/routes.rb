b4a888fc32459bb36be50232f353b52243ae70a5
Graham Moss <graham.moss@hotmail.com>
2017-02-07 14:38:00 -0500

    added restaurant controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  #get 'restaurants/index'
+
+  #get 'restaurants/show'
+
+  #get 'restaurants/new'
+
+  #get 'restaurants/edit'
+
+resources :restaurants, only: [:new, :show, :create]
+
 resources :users, only: [:new, :create]
 
 
