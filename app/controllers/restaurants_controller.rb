d0c348da828a9b7423fb25368736cf38819acef5
Graham Moss <graham.moss@hotmail.com>
2017-02-08 14:14:53 -0500

    added create method for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,4 +14,20 @@ class RestaurantsController < ApplicationController
   def edit
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      redirect_to '/'
+    end
+  end
+
+
+  def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :description, :image)
+    end
+
 end
