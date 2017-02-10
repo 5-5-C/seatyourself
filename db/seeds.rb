a49d6473fe3feb4b79431c3bf86beb76687941cc
5-5-C <jmarshall.brock@gmail.com>
2017-02-09 19:48:49 -0500

    added styling and html in layouts/application

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 
 
 Restaurant.create(
-name: "KFC",
+name: "Burger's Priest",
 address: "123 fake street",
 description: "chicken?",
 image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1024px-KFC_logo.svg.png",
 )
 
 Restaurant.create(
-name: "Wendy's",
+name: "W Burger",
 address: "321 fake street",
 description: "chicken? nah.",
 image: "http://images.canadianfreestuff.com/wp-content/uploads/2015/02/wendys-logo.png",
