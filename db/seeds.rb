45577eab6995f7c06461efebbd2d744f3d58ca4a
5-5-C <jmarshall.brock@gmail.com>
2017-02-10 12:35:31 -0500

    updated /restaurants index and added login yield

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,22 @@
 
 
 Restaurant.create(
-name: "Burger's Priest",
+name: "Grand Electric Toronto",
 address: "123 fake street",
-description: "chicken?",
-image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1024px-KFC_logo.svg.png",
+description: "Hip, bustling space for tacos, tunes & creative Mexican fare at communal tables.",
+image: "http://www.blogto.com/listings/restaurants/upload/2011/12/20111220-grand-electric-tacos.jpg",
 )
 
 Restaurant.create(
 name: "W Burger",
 address: "321 fake street",
-description: "chicken? nah.",
-image: "http://images.canadianfreestuff.com/wp-content/uploads/2015/02/wendys-logo.png",
+description: "Lively spot with a wide variety of burgers & toppings (along with salads & sandwiches), plus drinks.",
+image: "http://torontolivings.com/wp-content/uploads/2013/10/34.jpg",
+)
+
+Restaurant.create(
+name: "Electric Mudd",
+address: "321 fake street",
+description: "Lively spot with a wide variety of burgers & toppings (along with salads & sandwiches), plus drinks.",
+image: "http://cdn.torontolife.com/wp-content/uploads/2013/07/top-10-new-toronto-patios-intro.jpg",
 )
